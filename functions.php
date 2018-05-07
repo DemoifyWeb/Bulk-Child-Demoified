fcba6d84c18ec51c1bb5d93e08c378c0cb638034
dibyapandey/ <pandeydibya44@gmail.com>
2018-05-07 16:12:59 +0545

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+
+/*Adding font from links*/
+
+function wpb_load_fa() {
+ 
+wp_enqueue_style( 'wpb-fa', 'https://fonts.googleapis.com/css?family=Barlow' );
+ 
+}
+ 
+add_action( 'wp_enqueue_scripts', 'wpb_load_fa' );
+}
+?>
